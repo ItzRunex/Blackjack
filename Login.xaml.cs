0c72b53899a95b0b41a650f6eee562dba76558d1
m.dimitrov22 <m.dimitrov22@acsbg.org>
2022-02-24 12:21:52 +0200

    register

--- Login.xaml.cs
+++ Login.xaml.cs
@@ -27,31 +27,37 @@ namespace BlackjackGame
         }
         private void btnSubmit_Click(object sender, RoutedEventArgs e)
         {
-            string DBPath = @"LABSCIFIPC13\LOCALHOST";
+            string DBPath = @"LABSCIFIPC20\LOCALHOST";
             SqlConnection sqlCon = new SqlConnection($@"Data Source ={DBPath}; Initial Catalog = Accounts; Integrated Security = True");
-
             try
             {
                 if (sqlCon.State == ConnectionState.Closed)
                     sqlCon.Open();
-                string query = $"SELECT COUNT(1) FROM Accounts WHERE Username=@Username AND Password=@Password";
-                SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
-                sqlCmd.CommandType = CommandType.Text;
-                sqlCmd.Parameters.AddWithValue("@Username", txtUserName.Text);
-                sqlCmd.Parameters.AddWithValue("@Password", PasswordBox.Password);
-                int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
-                if (count == 1)
+                if (!CheckLogin(sqlCon))
                 {
-                    MainWindow dashboard = new MainWindow();
-                    dashboard.Show();
-                    this.Close();
-
+                    MessageBoxResult result = MessageBox.Show("No account matching! Do you want to register a new account?", "No Account", MessageBoxButton.YesNo);
+                    if (result == MessageBoxResult.Yes)
+                    {
+                        string regQuery = "INSERT INTO Accounts (Username, Password) VALUES (@Username, @Password)";
+                        SqlCommand sqlRegCmd = new SqlCommand(regQuery, sqlCon);
+                        sqlRegCmd.CommandType = CommandType.Text;
+                        sqlRegCmd.Parameters.AddWithValue("@Username", txtUserName.Text);
+                        sqlRegCmd.Parameters.AddWithValue("@Password", PasswordBox.Password);
+                        sqlRegCmd.ExecuteNonQuery();
+                        if (!CheckLogin(sqlCon))
+                        {
+                            MessageBox.Show("An error has occurred while creating your account!");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        return;
+                    }
                 }
-                else
-                {
-                    MessageBox.Show("No account matching! Do you want to register a new account?");
-
-                }
+                MainWindow dashboard = new MainWindow();
+                dashboard.Show();
+                this.Close();
             }
             catch (Exception ex)
             {
@@ -61,6 +67,16 @@ namespace BlackjackGame
             {
                 sqlCon.Close();
             }
+        }
+        private bool CheckLogin(SqlConnection sqlCon)
+        {
+            string query = "SELECT COUNT(1) FROM Accounts WHERE Username=@Username AND Password=@Password";
+            SqlCommand sqlCmd = new SqlCommand(query, sqlCon);
+            sqlCmd.CommandType = CommandType.Text;
+            sqlCmd.Parameters.AddWithValue("@Username", txtUserName.Text);
+            sqlCmd.Parameters.AddWithValue("@Password", PasswordBox.Password);
+            int count = Convert.ToInt32(sqlCmd.ExecuteScalar());
+            return count == 1;
         }
     }
 }
